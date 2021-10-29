dd1f01eedeb573ff1df70e659d679c4a683e7daf
葛红明 <gehm@ti-net.com.cn>
2021-10-29 09:21:49 +0800

    优化sh变量

--- interface.sh
+++ interface.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -ilex
-if docker ps -a | grep clink2_autotest$1 ;then
-	docker ps -a | grep clink2_autotest$1 | awk '{print $1}' | xargs docker stop
-	docker ps -a | grep clink2_autotest$1 | awk '{print $1}' | xargs docker rm
+if docker ps -a | grep clink2_autotest_$1 ;then
+	docker ps -a | grep clink2_autotest_$1 | awk '{print $1}' | xargs docker stop
+	docker ps -a | grep clink2_autotest_$1 | awk '{print $1}' | xargs docker rm
 	echo "rm container success!!!!!!!!!!"
 else
 	echo "no container!!!!!!!!!!!!"
 fi
-if docker images | grep clink2_autotest$1; then
-	docker images | grep clink2_autotest$1 | awk '{print $3}' | xargs docker rmi
+if docker images | grep clink2_autotest_$1; then
+	docker images | grep clink2_autotest_$1 | awk '{print $3}' | xargs docker rmi
 	echo "rm inages seccess!!!!!!!!!!!"
 else
 	echo "no images!!!!!!!!!!!!!!!!!"
 fi
 echo "start build!!!!!!!!!!!!!!!!"
-if docker build -t clink2_autotest$1 . -f $1;then
+if docker build -t clink2_autotest_$1 . -f $1;then
 
 	echo "build success!!!!!!!!!!!!1"
 else
@@ -21,5 +21,5 @@ else
 fi
 echo "make container!!!!!!!!!!!1"
 #docker run --name="clink2_autotest"  clink2_autotest
-docker run  -v /var/lib/jenkins/workspace/api_demo:/usr/src/clink2_autotest$1/ --name clink2_autotest$1  clink2_autotest$1
+docker run  -v /var/lib/jenkins/workspace/api_demo:/usr/src/clink2_autotest_$1/ --name clink2_autotest_$1  clink2_autotest_$1
 echo "complete!!!!!!!!!!!"
