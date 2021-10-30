030baa2cf442cb51732cc380d87cf3eab5d89600
gehongming <49710528+gehongming@users.noreply.github.com>
2021-10-30 16:22:29 +0800

    Update interface.sh

--- interface.sh
+++ interface.sh
@@ -12,23 +12,23 @@ if docker images | grep clink2_autotest_$1; then
 else
         echo "#######################no images#######################"
 fi
-sed -i "s/\${cases}/$1/g" call_success
+sed -i "s/\${cases}/$1/g" Dockerfile
 echo "start build!!!!!!!!!!!!!!!!"
 if docker build -t clink2_autotest_$1 .;then
 
         echo "#######################build success#######################"
 else
         echo "#######################build fail#######################"
-        sed -i "s/$1/\${cases}/g" call_success
+        sed -i "s/$1/\${cases}/g" Dockerfile
         exit 1
 fi
 echo "##########################make container###########################3"
 #docker run --name="clink2_autotest"  clink2_autotest
 if docker run  -v /var/lib/jenkins/workspace/api_demo:/usr/src/clink2_autotest_$1/ --name clink2_autotest_$1  clink2_autotest_$1; then
     echo "#######################complete##########################"
-    sed -i "s/$1/\${cases}/g" call_success
+    sed -i "s/$1/\${cases}/g" Dockerfile
 
 else
     echo "#######################container fail#######################"
 fi
-sed -i "s/$1/\${cases}/g" call_success
+sed -i "s/$1/\${cases}/g" Dockerfile
